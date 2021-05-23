205bd3641de11cfa2e859c3543fabf4e1b6cc52f
Marymezh <maria.mezhova@yahoo.com>
2021-05-23 08:06:49 +0700

    ProfileVIewController and ProfileHeaderVIew files were created, userPicture is downloaded

---
+++ Navigation/Profile/ProfileHeaderVIew.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderVIew.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 23.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+}
