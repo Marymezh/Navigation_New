c3dc82f5e3a838ba757513ae7ba9ef31da867b1b
Marymezh <maria.mezhova@yahoo.com>
2021-05-23 08:35:23 +0700

    task 1 - UIVIew was created at ProfileViewController, constraints created

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    private let header: ProfileHeaderView = ProfileHeaderView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .lightGray
-        view.addSubview(header)
-
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
-        header.frame = view.frame
-        
+
     }
 
 }
