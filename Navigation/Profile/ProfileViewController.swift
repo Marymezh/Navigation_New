eb5547e3615d9f82d3dc9eaafb3ab182e7d69d85
Marymezh <maria.mezhova@yahoo.com>
2021-05-23 09:07:48 +0700

    task 3 - newButton layout was changed

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -26,15 +26,11 @@ class ProfileViewController: UIViewController {
             newButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
             newButton.setTitleColor(.white, for: .normal)
             newButton.layer.backgroundColor = UIColor.blue.cgColor
-            newButton.layer.cornerRadius = 14
-            newButton.layer.shadowColor = UIColor.black.cgColor
-            newButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-            newButton.layer.shadowOpacity = 0.7
             newButton.translatesAutoresizingMaskIntoConstraints = false
-            newButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-            newButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: +16).isActive = true
-            newButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
-            newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
+        NSLayoutConstraint.activate([newButton.heightAnchor.constraint(equalToConstant: 50),
+                                 newButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 0),
+                                 newButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+                                 newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: 0)])
     }
     
     override func viewWillLayoutSubviews() {
