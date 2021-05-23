f06d8a5633e62640478bb7396281b9717f01f327
Marymezh <maria.mezhova@yahoo.com>
2021-05-23 08:15:57 +0700

    Profile picture was changed, ProfileHeaderView is up to the task (all the UI elements were created, frames and layout were configured)

--- Navigation/Profile/ProfileHeaderVIew.swift
+++ Navigation/Profile/ProfileHeaderVIew.swift
@@ -10,4 +10,60 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
+    private let userPicture = UIImageView(image: UIImage(named: "mysterious-cat.jpg"))
+    private let userName = UILabel()
+    private let userStatus = UILabel()
+    private let statusButton = UIButton()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        configureFrames()
+    }
+    
+    func setupUI () {
+        addSubview(userPicture)
+        userPicture.layer.borderWidth = 3.0
+        userPicture.layer.borderColor = UIColor.white.cgColor
+        userPicture.clipsToBounds = true
+        
+        addSubview(userName)
+        userName.text = "Mysterious Cat"
+        userName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        userName.textColor = .black
+        
+        addSubview(userStatus)
+        userStatus.text = "Learning how to code..."
+        userStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        userStatus.textColor = .gray
+        
+        addSubview(statusButton)
+        statusButton.setTitle("Show status", for: .normal)
+        statusButton.setTitleColor(.white, for: .normal)
+        statusButton.layer.backgroundColor = UIColor.blue.cgColor
+        statusButton.layer.cornerRadius = 14
+        statusButton.layer.shadowColor = UIColor.black.cgColor
+        statusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        statusButton.layer.shadowOpacity = 0.7
+        statusButton.addTarget(self, action: #selector(buttonIsPressed), for: .touchUpInside)
+    }
+    
+    func configureFrames() {
+        userPicture.frame = CGRect(x: self.safeAreaInsets.left + 16, y: self.safeAreaInsets.top + 16, width: 110, height: 110)
+        userPicture.layer.cornerRadius = userPicture.frame.size.width/2
+        userName.frame = CGRect(x: self.safeAreaInsets.left + 150, y: self.safeAreaInsets.top + 27, width: 300, height: 18)
+        userStatus.frame = CGRect(x: self.safeAreaInsets.left + 150, y: statusButton.frame.minY - userStatus.bounds.height - 34, width: 300, height: 14)
+        statusButton.frame = CGRect(x: self.safeAreaInsets.left + 16, y: userPicture.frame.maxY + 16 , width: self.bounds.width - self.safeAreaInsets.left - self.safeAreaInsets.right - 16 * 2, height: 50)
+    }
+    
+    @objc func buttonIsPressed() {
+        print (userStatus.text ?? "Status is not available")
+    }
 }
