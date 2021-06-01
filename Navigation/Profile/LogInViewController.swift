989ec34ace6bed0355e216da24026c5588aaf714
Marymezh <maria.mezhova@yahoo.com>
2021-06-01 16:05:25 +0700

    tasks 2-6 - LogInViewController was created and attached to navigation, navigation bar is hidden

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 01.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
