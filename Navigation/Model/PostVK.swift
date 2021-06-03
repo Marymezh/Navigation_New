6d97cb6aa628c3844fc2c457fd3457b2637f0882
Marymezh <maria.mezhova@yahoo.com>
2021-06-03 10:22:19 +0700

    tasks 1 - 3 PostVK model is created, PostStorage is created

---
+++ Navigation/Model/PostVK.swift
@@ -0,0 +1,15 @@
+//
+//  PostVK.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 03.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+struct PostVK {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,37 +9,6 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    private(set) lazy var newButton: UIButton = {
-        let newButton = UIButton()
-        newButton.setTitle("NEW BUTTON", for: .normal)
-        return newButton
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        navigationController?.navigationBar.isHidden = true
-        view.backgroundColor = .lightGray
-        configureButton()
-    }
-    
-    private func configureButton() {
-        self.view.addSubview(newButton)
-        newButton.setTitle("NEW BUTTON", for: .normal)
-        newButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        newButton.setTitleColor(.white, for: .normal)
-        newButton.layer.backgroundColor = UIColor.blue.cgColor
-        newButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let newbuttonConstraints = [
-            newButton.heightAnchor.constraint(equalToConstant: 50),
-            newButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            newButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            newButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)]
-        
-        NSLayoutConstraint.activate(newbuttonConstraints)
-        
-    }
+
 }
 
