ab626f29bf079691cdad67857d9af357df75adf7
Marymezh <maria.mezhova@yahoo.com>
2021-06-08 10:01:55 +0700

    task 1 - photos for the gallery are added to the Assets, PhotosTableViewCell.swift file is created

---
+++ Navigation/Photogallery/PhotosTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosTableViewCell.swift
+//  Navigation
+//
+//  Created by Maria Mezhova on 08.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UICollectionViewCell {
+    
+}
