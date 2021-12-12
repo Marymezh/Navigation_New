9ea9bbf0540064674549e22c41c529bbaf6e1bcf
Marymezh <maria.mezhova@yahoo.com>
2021-12-12 14:51:44 +0700

    task 1 - authorization errors handler with do-catch block

--- Navigation/Veryfication and LogIn/View/LogInViewController.swift
+++ Navigation/Veryfication and LogIn/View/LogInViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AuthorizationError: Error {
+    case empty
+    case incorrect
+}
+
 class LogInViewController: UIViewController {
     
     // ДЗ 4.1 
@@ -83,36 +88,49 @@ class LogInViewController: UIViewController {
     
     private lazy var logInButton: MyCustomButton = {
         let button = MyCustomButton(title: "Log in", titleColor: .white, backgroundColor: nil, backgroundImage: #imageLiteral(resourceName: "blue_pixel")) { [self] in
-            
-            #if DEBUG
-            let userService = TestUserService()
-            #else
-            let userService = CurrentUserService()
-            #endif
-            
-            if let username = self.usernameTextField.text,
-               let pswd = self.passwordTextField.text,
-               let inspector = self.loginFactory?.produceLoginInspector,
-               inspector().checkTextFields(login: username, password: pswd) == true {
-// navigate to another screen using weak link to coordinator
-//                self.coordinator?.showProfileVC(userService: userService, username: username)
-                self.pushProfile?(userService, username)
-            } else {
-                self.showAlert()
+            do {
+                try self.performLogin()
+            } catch AuthorizationError.empty {
+                self.showAlert(message: "Do not live blank fields!")
+            } catch AuthorizationError.incorrect {
+                self.showAlert(message: "User name or password is invalid")
+            } catch {
+                self.showAlert(message: "Unexpected error")
             }
         }
-        
         button.layer.cornerRadius = 10
         button.clipsToBounds = true
         return button
     }()
     
-    private func showAlert() {
-        let alertController = UIAlertController(title: "ERROR", message: "User name or password is invalid", preferredStyle: .alert)
+    private func performLogin() throws {
+        
+        #if DEBUG
+        let userService = TestUserService()
+        #else
+        let userService = CurrentUserService()
+        #endif
+        
+        guard usernameTextField.text != "" || passwordTextField.text != "" else {
+            throw AuthorizationError.empty
+        }
+        guard let username = usernameTextField.text,
+              let pswd = passwordTextField.text,
+              let inspector = loginFactory?.produceLoginInspector,
+              inspector().checkTextFields(login: username, password: pswd) == true else {
+            throw AuthorizationError.incorrect
+        }
+
+        // navigate to another screen using weak link to coordinator
+        //                self.coordinator?.showProfileVC(userService: userService, username: username)
+        pushProfile?(userService, username)
+    }
+    
+    private func showAlert(message: String) {
+        let alertController = UIAlertController(title: "ERROR", message: message, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Chancel", style: .default, handler: nil)
         alertController.addAction(cancelAction)
         self.present(alertController, animated: true, completion: nil)
-        print("invalid name")
     }
     
     override func viewDidLoad() {
